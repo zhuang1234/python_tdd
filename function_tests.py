548dfda95263c80a22561660dcbfa5d706c645d6
nashan <z929346631@gmail.com>
2021-11-25 20:54:59 +0800

    save

--- function_tests.py
+++ function_tests.py
@@ -35,20 +35,26 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车键后页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(3)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows =table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # 页面中又显示了一个文本框可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly”使用孔雀羽毛做假蝇
         # 伊迪丝做事很有条理
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
 
 
         # 页面再次更新她的清单中显示了这两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows =table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # 伊迪丝想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -13,9 +14,28 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
     
     def test_home_page_returns_correct_html(self):
+        '''
         request = HttpRequest()
         response = home_page(request)
+        '''
+        response = self.client.get('/')
         html =response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+
+        if False:
+            expected_html = render_to_string('home.html')
+            self.assertEqual(html, expected_html)
+        else:
+            self.assertTrue(html.startswith('<html>'))
+            self.assertIn('<title>To-Do lists</title>', html)
+            self.assertTrue(html.strip().endswith('</html>'))
+
+            self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_home_template(self):
+        response =self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response =self.client.post('/',data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
