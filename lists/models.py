9a10dc62332285cf9549a12c058cb73b8cf742b4
nashan <z929346631@gmail.com>
2021-11-27 16:55:24 +0800

    7.9 调整模型，让待办事项和不同的清单关联起来

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,31 @@
 from django.db import models
-from django.db.models.fields import TextField
 
 # Create your models here.
+
+class List(models.Model):
+    pass
+
+
+## 若想保存对象之间的关系要告诉 Django 两个类之间的关系这种关系使用 ForeignKey 字段表示
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default='', on_delete=models.CASCADE)
+
+    """django 升级到2.0之后,表与表之间关联的时候,必须要写on_delete参数,否则会报异常
+        on_delete=None,               # 删除关联表中的数据时,当前表与其关联的field的行为
+	    on_delete=models.CASCADE,     # 删除关联数据,与之关联也删除
+	    on_delete=models.DO_NOTHING,  # 删除关联数据,什么也不做
+	    on_delete=models.PROTECT,     # 删除关联数据,引发错误ProtectedError
+	    # models.ForeignKey('关联表', on_delete=models.SET_NULL, blank=True, null=True)
+	    on_delete=models.SET_NULL,    # 删除关联数据,与之关联的值设置为null（前提FK字段需要设置为可空,一对一同理）
+	    models.ForeignKey('关联表', on_delete=models.SET_DEFAULT, default='默认值')
+	    on_delete=models.SET_DEFAULT, # 删除关联数据,与之关联的值设置为默认值（前提FK字段需要设置默认值,一对一同理）
+	    on_delete=models.SET,         # 删除关联数据,
+	    a. 与之关联的值设置为指定值,设置：models.SET(值)
+	    b. 与之关联的值设置为可执行对象的返回值,设置：models.SET(可执行对象)
+     """
+
+
+## 删除迁移是种危险操作但偶尔需要这么做因为不可能从一开始就正确定义模型。
+## 如果删除已经用于某个数据库的迁移Django 就不知道当前状态因此也就不知道如何运行以后的迁移。
+## 只有当你确定某个迁移没被使用时才能将其删除。根据经验已经提交到 VCS 的迁移决不能删除
--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest, response
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item,List
 
 from lists.views import home_page
 
@@ -53,23 +53,33 @@ class HomePageTest(TestCase):
         self.assertIn('itemey 2', response.content.decode())
     """
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_item(self):
+        list_ =List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class LiveViewTest(TestCase):
 
@@ -78,8 +88,9 @@ class LiveViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text = 'itemey 1')
-        Item.objects.create(text = 'itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text = 'itemey 1', list=list_)
+        Item.objects.create(text = 'itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
