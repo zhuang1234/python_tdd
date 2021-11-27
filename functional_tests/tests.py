ea201174efef4028816942bdaf198d377cb28889
nashan <z929346631@gmail.com>
2021-11-27 12:28:01 +0800

    把功能测试集成到django中，使用LiveServertestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+# class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
     
@@ -18,7 +20,8 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retereve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
-        self.browser.get('http://localhost:8000')
+        # self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 她注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
@@ -66,5 +69,6 @@ class NewVisitorTest(unittest.TestCase):
         # 她很满意去睡觉了
         self.fail('Finish the test!')
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+#之后都使用 Django 的测试运行程序运行功能测试。可以删除
+# if __name__ == '__main__':
+#     unittest.main(warnings='ignore')
