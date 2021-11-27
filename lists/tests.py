0f349c2085f1b942d0243c3b95fee1117b18489c
nashan <z929346631@gmail.com>
2021-11-27 16:04:25 +0800

    7.7新的界面去展示list

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
-from django.http import HttpRequest
+from django.http import HttpRequest, response
 from django.template.loader import render_to_string
 from lists.models import Item
 
@@ -9,11 +9,11 @@ from lists.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
-
+    '''
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-    
+    '''
     def test_home_page_returns_correct_html(self):
         '''
         request = HttpRequest()
@@ -47,12 +47,12 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response =self.client.post('/',data={'item_text':'A new list item'}) 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
+    """Noused
     def test_displays_all_list_items(self):
         Item.objects.create(text = 'itemey 1')
         Item.objects.create(text = 'itemey 2')
@@ -61,6 +61,7 @@ class HomePageTest(TestCase):
 
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
+    """
 
 class ItemModelTest(TestCase):
 
@@ -79,3 +80,26 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+class LiveViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text = 'itemey 1')
+        Item.objects.create(text = 'itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        # self.assertIn('itemey 1', response.content.decode())
+        # self.assertIn('itemey 2', response.content.decode())  
+        ## Django 提供了 assertContains 方法它知道如何处理响应以及响应内容中的字节 
+        ## assertContains 的附加好处————它直接指出测试失败的原因
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+
+
+
--- lists/views.py
+++ lists/views.py
@@ -6,9 +6,13 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text = request.POST['item_text'])
         # .objects.create 是创建新 Item 对象的简化方式无须再调用 .save() 方法
-        return redirect('/')
-
-    items = Item.objects.all()
-
+        return redirect('/lists/the-only-list-in-the-world/')
+    
+    #items = Item.objects.all()
     # render第一个参数是请求对象、第二个参数是渲染的模板名、函数的第三个参数是一个字典把模板变量的名称映射在值上
-    return render(request, 'home.html', {'items': items})
+    # return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
