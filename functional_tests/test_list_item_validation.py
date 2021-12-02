4a25de56440202abbeb6e01276f7d41891a3f705
nashan <z929346631@gmail.com>
2021-12-02 21:00:02 +0800

    “完善功能测试，不允许提交空的待办事项
    ”

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -13,26 +13,33 @@ MAX_WAIT = 10
 
 class ItemValidationTest(FunctionalTest):
     ## unittest提供的修饰器 @skip 可以临时禁止执行这个测试方法
-    @skip
+    # @skip
     def test_cannot_add_empty_list_items(self):
         # 伊迪丝访问首页不小心提交了一个空待办事项
         # 输入框中没输入内容她就按下了回车键
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
 
         # 首页刷新了显示一个错误消息
         # 提示待办事项不能为空
-
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
         # 她输入一些文字然后再次提交这次没问题了
-
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
         # 她有点儿调皮又提交了一个空待办事项
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         # 在清单页面她看到了一个类似的错误消息
-
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
         # 输入文字之后就没问题了
-        self.fail('write me!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
 
-
-
-
-# fail('Finish the test!')
-#之后都使用 Django 的测试运行程序运行功能测试。可以删除
-# if __name__ == '__main__':
-#     unittest.main(warnings='ignore')
